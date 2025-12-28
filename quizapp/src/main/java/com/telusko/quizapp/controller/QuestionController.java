016426b3adb666c7f39f6605cc156883d08d8f29
aayanksinghai <aayanksinghai02@gmail.com>
2025-12-28 16:44:40 +0530

    Quiz question dao service setup

--- quizapp/src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -1,15 +1,23 @@
 package com.telusko.quizapp.controller;
 
+import com.telusko.quizapp.entity.Question;
+import com.telusko.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
 
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
-        return "Hi, These are your questions";
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
     }
 }
