9de99b2b6b08ef98b08fd4f0bf9350a976db60f4
aayanksinghai <aayanksinghai02@gmail.com>
2026-01-09 19:34:23 +0530

    Monolithic Quiz App Impl

--- quizapp/src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -1,8 +1,11 @@
 package com.telusko.quizapp.controller;
 
 import com.telusko.quizapp.entity.Question;
+import com.telusko.quizapp.entity.QuestionWrapper;
 import com.telusko.quizapp.service.QuestionService;
+import com.telusko.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,18 +17,22 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
 
+    @Autowired
+    QuizService quizService;
+
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable  String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable  String category){
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
     }
+
 }
---
+++ quizapp/src/main/java/com/telusko/quizapp/entity/Response.java
@@ -0,0 +1,28 @@
+package com.telusko.quizapp.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Response {
+    private Integer id;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String response;
+}
