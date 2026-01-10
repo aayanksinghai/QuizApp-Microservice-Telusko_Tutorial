14251c5a464f69b1160c34b06ca9a76d5488ee11
aayanksinghai <aayanksinghai02@gmail.com>
2026-01-10 12:23:00 +0530

    Impl Question Service

--- question-service/src/main/java/com/telusko/question_service/controller/QuestionController.java
+++ question-service/src/main/java/com/telusko/question_service/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package com.telusko.question_service.controller;
 
 import com.telusko.question_service.entity.Question;
+import com.telusko.question_service.entity.QuestionWrapper;
+import com.telusko.question_service.entity.Response;
 import com.telusko.question_service.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -33,5 +35,20 @@ public class QuestionController {
 //    generate
 //    getQuestions(questionId)
 //    getScore
+
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(@RequestParam String categoryName, @RequestParam Integer numQuestions){
+        return questionService.getQuestionForQuiz(categoryName,  numQuestions);
+    }
+
+    @GetMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds){
+        return questionService.getQuestionFromId(questionIds);
+    }
+
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses){
+        return questionService.getScore(responses);
+    }
 
 }
--- question-service/src/main/java/com/telusko/question_service/service/QuestionService.java
+++ question-service/src/main/java/com/telusko/question_service/service/QuestionService.java
@@ -2,6 +2,8 @@ package com.telusko.question_service.service;
 
 import com.telusko.question_service.dao.QuestionDao;
 import com.telusko.question_service.entity.Question;
+import com.telusko.question_service.entity.QuestionWrapper;
+import com.telusko.question_service.entity.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,5 +48,43 @@ public class QuestionService {
         }
         return new ResponseEntity<>(new String(), HttpStatus.BAD_REQUEST);
 
+    }
+
+    public ResponseEntity<List<Integer>> getQuestionForQuiz(String categoryName, Integer numQuestions) {
+        List<Integer> questions = questionDao.findRandomQuestionsByCategory(categoryName, numQuestions);
+        return new ResponseEntity<>(questions, HttpStatus.OK);
+    }
+
+
+    public ResponseEntity<List<QuestionWrapper>> getQuestionFromId(List<Integer> questionIds) {
+        List<QuestionWrapper> wrappers = new ArrayList<>();
+        List<Question> questions = new ArrayList<>();
+        for(Integer id: questionIds){
+            questions.add(questionDao.findById(id).get());
+        }
+
+        for(Question question : questions){
+            QuestionWrapper questionWrapper = new QuestionWrapper();
+            questionWrapper.setId(question.getId());
+            questionWrapper.setQuestionTitle(question.getQuestionTitle());
+            questionWrapper.setOption1(question.getOption1());
+            questionWrapper.setOption2(question.getOption2());
+            questionWrapper.setOption3(question.getOption3());
+            questionWrapper.setOption4(question.getOption4());
+            wrappers.add(questionWrapper);
+        }
+        return new ResponseEntity<>(wrappers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> getScore(List<Response> responses) {
+
+        int right = 0;
+        for(Response response : responses){
+            Question question = questionDao.findById(response.getId()).get();
+            if(response.getResponse().equals(question.getRightAnswer())){
+                right++;
+            }
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
     }
 }
