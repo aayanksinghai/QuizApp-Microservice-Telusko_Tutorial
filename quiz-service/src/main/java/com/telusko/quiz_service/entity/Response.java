14b297fa6d0917f78585c7184a83a0c12edf7d46
aayanksinghai <aayanksinghai02@gmail.com>
2026-01-12 17:32:57 +0530

    Setup Eureka Server

---
+++ quiz-service/src/main/java/com/telusko/quiz_service/entity/Response.java
@@ -0,0 +1,28 @@
+package com.telusko.quiz_service.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Response {
+    private Integer id;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String response;
+}
---
+++ quiz-service/src/main/java/com/telusko/quiz_service/service/QuizService.java
@@ -0,0 +1,64 @@
+package com.telusko.quiz_service.service;
+
+import com.telusko.quiz_service.dao.QuizDao;
+import com.telusko.quiz_service.entity.QuestionWrapper;
+import com.telusko.quiz_service.entity.Quiz;
+import com.telusko.quiz_service.entity.Response;
+import com.telusko.quizapp.dao.QuestionDao;
+import com.telusko.quizapp.dao.QuizDao;
+import com.telusko.quizapp.entity.Question;
+import com.telusko.quizapp.entity.QuestionWrapper;
+import com.telusko.quizapp.entity.Quiz;
+import com.telusko.quizapp.entity.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuizService {
+
+    @Autowired
+    QuizDao quizDao;
+
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+         List<Integer> questions = //call the generate url
+//
+//        Quiz quiz = new Quiz();
+//        quiz.setTitle(title);
+//        quiz.setQuestions(questions);
+//
+//        quizDao.save(quiz);
+        return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+//        Optional<Quiz> quiz = quizDao.findById(id);
+//        List<Question> questionFromDB = quiz.get().getQuestions();
+          List<QuestionWrapper> questionsForUser = new ArrayList<>();
+//
+//        for(Question q : questionFromDB){
+//            QuestionWrapper questionWrapper = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+//            questionsForUser.add(questionWrapper);
+//        }
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+          Quiz quiz = quizDao.findById(id).get();
+//        List<Question> questions = quiz.getQuestions();
+//        int right = 0;
+//        int i  = 0;
+//        for(Response response : responses){
+//            if(response.getResponse().equals(questions.get(i).getRightAnswer())){
+//                right++;
+//            }
+//            i++;
+//        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
+    }
+}
